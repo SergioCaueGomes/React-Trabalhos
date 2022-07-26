9df5116e75173bc32d433b8c444870521ca2ff61
SergioCaueGomes <caue.sergio@gmail.com>
2022-07-26 03:07:00 -0300

    Commit UseRef exercio 1/2

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -2,14 +2,28 @@ import React, { useRef, useState, useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
+const merge = function(s1, s2) {
+    return [...s1].map((e, i) => `${e}${s2[i] || ""}`).join("")
+}
+
 const UseRef = (props) => {
     const [value1, setValue1] = useState("")
+    const [value2, setValue2] = useState("")
+    
     const count = useRef(0)
+    const myInput1 = useRef(null)
+    const myInput2 = useRef(null)
 
     useEffect(function(){
         count.current = count.current + 1
+        myInput2.current.focus()
     }, [value1])
 
+    useEffect(function(){
+        count.current = count.current++
+        myInput1.current.focus()
+    }, [value2])
+    
     return (
         <div className="UseRef">
             <PageTitle
@@ -21,12 +35,21 @@ const UseRef = (props) => {
             <div className="center">
                 <div>
                    <span className="text">Valor: </span>
-                   <span className="text">{value1} [</span>
+                   <span className="text">{merge(value1, value2)} [</span>
                    <span className="text red">{count.current}</span>
                    <span className="text">]</span>
                 </div>
                 <input type="text" className="input" 
-                value={value1} onChange={e => setValue1(e.target.value)}/>
+                    ref={myInput1}
+                    value={value1} onChange={e => setValue1(e.target.value)} />
+            </div>
+
+             <SectionTitle title="Exercicio #02" />
+            
+             <div className="center">
+                <input type="text" className="input" 
+                ref={myInput2}
+                value={value2} onChange={e => setValue2(e.target.value)} />
             </div>    
         </div>
     )
