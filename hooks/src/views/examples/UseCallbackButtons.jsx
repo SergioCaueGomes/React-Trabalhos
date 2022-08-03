986c300026a0361d49fb7d995f06f77787b543d6
SergioCaueGomes <caue.sergio@gmail.com>
2022-08-03 15:53:26 -0300

    Commit iniciando Context API

--- hooks/src/views/examples/UseCallbackButtons.jsx
+++ hooks/src/views/examples/UseCallbackButtons.jsx
@@ -4,13 +4,20 @@ const UseCallbackButtons = (props) => {
     return (
         <div>
             <button className='btn'
-                onClick={() => props.inc(6)}>+6</button>
+                onClick={() => props.inc(6)}>
+                +6
+            </button>
             <button className='btn'
-                onClick={() => props.inc(12)}>+12</button>
+                onClick={() => props.inc(12)}>
+                +12
+            </button>
             <button className='btn'
-                onClick={() => props.inc(18)}>+18</button>
+                onClick={() => props.inc(18)}>
+                +18
+            </button>
         </div>
     )
 }
 
 export default React.memo(UseCallbackButtons)
+
